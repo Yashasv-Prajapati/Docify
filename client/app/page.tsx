7b83e787423bb67f690473a97fab12992eb662c7
Yashasav Prajapati <yamuprajapati05@gmail.com>
2024-02-19 15:18:17 +0530

    github auth with user repository list

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
+import { buttonVariants } from '@/components/ui/button';
 import Wrapper from '@/components/wrapper';
 
 export default function Home() {
   return (
     <>
-      <Wrapper>Hello this is docify</Wrapper>
+      <Wrapper>
+        Hello this is docify
+        <Link
+          className={buttonVariants({
+            size: 'sm',
+          })}
+          href={`https://github.com/user/installations/{installation_id}/repositories?client_id=${process.env.GITHUB_CLIENT_ID}`}
+        >
+          Authorize
+        </Link>
+      </Wrapper>
     </>
   );
 }
