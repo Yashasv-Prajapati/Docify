2e88decb98ce8b7cde93ed4dcf044bf56fc7541f
Anushtha <anushthaprakash@gmail.com>
2024-02-19 19:48:11 +0530

    changes requests resolved

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 import './globals.css';
 
-
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Docify',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body >{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx,js}',
-    './components/**/*.{ts,tsx,js}',
-    './app/**/*.{ts,tsx,js}',
-    './src/**/*.{ts,tsx,js}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
 	],
   prefix: "",
   theme: {
