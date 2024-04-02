9ab73062783a349e4f0330a70d1264058a4d4593
Shobhit Juglan <shojug123@gmail.com>
2024-04-02 16:40:03 +0530

    Fixed dependency checker(java) to detect only external libraries

---
+++ java/dependency_checker/test_folder/Test2.java
@@ -0,0 +1,8 @@
+import java.net.url;
+import a.Test3;
+
+public class Test2 {
+   public static void main(String[] args) {
+        System.out.println("Hello World!"); //Display the string.
+    }
+}
