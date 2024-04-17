e77578e418c9580b749930450e33f57e9f4160f7
Harshit Gupta <2021csb1092@iitrpr.ac.in>
2024-04-17 20:03:24 +0530

    project card

--- client/app/dashboard/_components/project-card.tsx
+++ client/app/dashboard/_components/project-card.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { FC } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { GitBranchIcon, GitCommitIcon, MoreHorizontalIcon } from 'lucide-react';
+import { GitBranchIcon, GitCommitIcon,Github, MoreHorizontalIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -63,33 +64,35 @@ const ProjectCard: FC<ProjectCardProps> = ({
       </div>
       <Separator className='my-2 lg:hidden' />
       <div className='grid flex-1 gap-1 p-2'>
-        <div className='flex items-start gap-2'>
-          <span
+        <div className=' my-2 flex items-start gap-2'>
+        <svg width="24" height="24" viewBox="0 0 24 24" 
+        fill="none" stroke="currentColor" strokeWidth="2" 
+        strokeLinecap="round" strokeLinejoin="round" 
+        className="lucide lucide-github">
+          <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/>
+          <path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+            <Link href={url} className='hover:underline'>Github</Link>
+        </div>
+      </div>
+      <Separator className='my-2 lg:hidden' />
+      <div className='grid flex-1 gap-1 p-2'>
+        <div className='flex items-center gap-2'>
+          
+          Project type
+        </div>
+        <div className='flex items-center gap-2'>
+        <span
             className={`inline-flex size-3 translate-y-1 rounded-full bg-green-400`}
           />
-          <div>
-            Ready
-            <div className='text-gray-500 dark:text-gray-400'>2m 35s</div>
-          </div>
+          {project_type}
         </div>
       </div>
       <Separator className='my-2 lg:hidden' />
-      <div className='grid flex-1 gap-1 p-2'>
-        <div className='flex items-center gap-2'>
-          <GitBranchIcon className='size-4' />
-          {testing_dir}
-        </div>
-        <div className='flex items-center gap-2'>
-          <GitCommitIcon className='size-4' />
-          <span className='line-clamp-1'>commit</span>
-        </div>
-      </div>
-      <Separator className='my-2 lg:hidden' />
-      <div className='grid flex-1 gap-1 p-2'>
+      {/* <div className='grid flex-1 gap-1 p-2'>
         <div className='flex items-center gap-2 text-gray-500 dark:text-gray-400'>
           date
         </div>
-      </div>
+      </div> */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
