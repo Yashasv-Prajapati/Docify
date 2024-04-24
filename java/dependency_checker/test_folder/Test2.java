850a46a33ee8f69449c3c1ad3ffd5d66b85c73ba
5h0bh1t <95950129+shobhitjuglan@users.noreply.github.com>
2024-04-24 18:13:19 +0530

    Modified api route for dependency checker (#150)
    * Modified api route for dependency checker
    
    * Some changes

--- java/dependency_checker/test_folder/Test2.java
+++
@@ -1,8 +0,0 @@
-import java.net.url;
-import a.Test3;
-
-public class Test2 {
-   public static void main(String[] args) {
-        System.out.println("Hello World!"); //Display the string.
-    }
-}
