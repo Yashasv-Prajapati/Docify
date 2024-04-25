16b34fa55a4452ea0c53528d0fab923b6a866a5d
Sarvottam <sarvottamswaroop100@gmail.com>
2024-04-25 19:07:44 +0530

    removed env variables

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,17 +1,5 @@
 // /jest.setup.ts
 import '@testing-library/jest-dom';
-// require('dotenv').config({ path: '.env.test' });
-process.env.GITHUB_CLIENT_ID='Iv1.1b5aed7d626fad11'
-process.env.GITHUB_CLIENT_SECRET='2e0d7464635921fac52e047a621a285109b8fc0f'
-process.env.GITHUB_APP_ID='824467'
-process.env.GITHUB_ACCESS_TOKEN='ghu_l0OSEBAn0Xbf75589ugBJjmwbdPDkw3srg0p'
-process.env.GITHUB_REDIRECT_URI = 'http://localhost:3000/api/auth'
-process.env.NEXT_PUBLIC_APP_ID='824467'
-process.env.NEXT_SERVER_URL='http://localhost:3000'
-process.env.NEXTAUTH_SECRET='5apRdFz6FsbQzXtEp6ume1tkF4RWa+EK4G+JtK2jAq0='
-process.env.DATABASE_URL='file:./dev.db'
-process.env.JWT_SECRET="secretkey"
-
 
 // import { PrismaClient } from '@prisma/client'
 // import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
