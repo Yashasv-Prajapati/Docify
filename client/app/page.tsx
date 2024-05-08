013c6a196c224c5506c05298be90f2dc9e74e5fa
ashima-09 <96876403+ashima-09@users.noreply.github.com>
2024-05-08 20:08:31 +0530

    final unit tests for home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,53 +1,24 @@
+
 'use client';
 
 import Image from 'next/image';
 
-import MarkdownEditor from '@/components/markdownEditor';
-
-import { Footer, Navbar } from '../components/index';
-import {
-  About,
-  Explore,
-  GetStarted,
-  Hero,
-  WhatsNew,
-} from '../components/landingPage';
+import Navbar from '@/components/LandingPageNew/navbar/page'
+import FirstSection from '@/components/LandingPageNew/first-section/page';
+import SecondSection from '@/components/LandingPageNew/second-section/page';
 
 const Home = () => {
   return (
     <div>
-      <div className='overflow-hidden bg-[#d1d4db]'>
-        <div data-testid='navbar'>
-          <Navbar />
-        </div>
-        <div data-testid="hero">
-          <Hero />
-        </div>
-        {/* <Navbar/> */}
-        {/* <Hero/> */}
-        <div className='relative'>
-          <div data-testid="about">
-            <About />
-          </div>
-          <div className='gradient-03 z-0' />
-          <div data-testid="explore">
-            <Explore />
-          </div>
-        </div>
-        <div className='relative'>
-          <div data-testid="get-started">
-            <GetStarted />
-          </div>
-          <div className='gradient-04 z-0' />
-          <div data-testid="whats-new">
-            <WhatsNew />
-          </div>
-        </div>
-        <div data-testid="footer">
-          <Footer />
-        </div>
+      <div data-testid ="navbar"> 
+      <Navbar type ="home"/>
       </div>
-      {/* <MarkdownEditor /> */}
+      <div data-testid ="firstsection"> 
+      <FirstSection/>
+      </div>
+      <div data-testid ="secondsection"> 
+      <SecondSection/>
+      </div>
     </div>
   );
 };
